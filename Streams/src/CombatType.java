20c3ef5f0a043f3969735647c8d6d3a5631a748f
unknown <tobias.rebenda@aon.at>
2024-10-01 14:41:02 +0200

    Aufgabe 1 erledigt

---
+++ Streams/src/CombatType.java
@@ -0,0 +1,3 @@
+public enum CombatType {
+    MELEE, RANGED, NONE
+}
---
+++ Streams/src/Weapon.java
@@ -0,0 +1,19 @@
+public class Weapon {
+    private String name;
+    private CombatType combatType;
+    private DamageType damageType;
+    private int damage;
+    private int speed;
+    private int strength;
+    private int value;
+
+    public Weapon(String name, CombatType combatType, DamageType damageType, int damage, int speed, int strength, int value) {
+        this.name = name;
+        this.combatType = combatType;
+        this.damageType = damageType;
+        this.damage = damage;
+        this.speed = speed;
+        this.strength = strength;
+        this.value = value;
+    }
+}
